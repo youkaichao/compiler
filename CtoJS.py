b1ebfa7a7945b96fd6938b8b6a2462ec6a57a39d
Koala.H.F <fengh16@163.com>
2018-12-22 02:01:39 +0800

    Update C to JS part

--- CtoJS.py
+++ CtoJS.py
@@ -6,108 +6,57 @@ from CVisitor import CVisitor
 
 
 def addIndex(a, num=2):
-    '\n'.join([' ' * num + i for i in a.split('\n')])
+    return '\n'.join([' ' * num + i for i in a.split('\n')])
 
 
 class ToJSVisitor(CVisitor):
     def visitCompilationUnit(self, ctx):
-        return self.visit(ctx.translationUnit())
-
-    def visitTranslationUnit(self, ctx):
-        if ctx.translationUnit():
-            return self.visit(ctx.translationUnit()) + '\n' + self.visit(ctx.externalDeclaration())
-        return self.visit(ctx.externalDeclaration())
-
-    def visitExternalDeclaration(self, ctx):
-        if ctx.functionDefinition():
-            return self.visit(ctx.functionDefinition())
-        elif ctx.declaration():
-            return self.visit(ctx.declaration())
-        return ctx.getText()
+        return '\n'.join([self.visit(i) for i in ctx.children[:-1]])
 
     def visitFunctionDefinition(self, ctx):
-        # ans = ""
-        # if ctx.declarationSpecifiers():
-        #     ans += self.visit(ctx.declarationSpecifiers())
+        functionname = self.visit(ctx.declarator())
+        if functionname == 'main':
+            return self.visitCompoundStatementInMain(ctx.compoundStatement())
         ans = 'function'
         ans += ' ' + self.visit(ctx.declarator())
-        if ctx.declarationList():
-            ans += ' ' + self.visit(ctx.declarationList())
         ans += ' ' + self.visit(ctx.compoundStatement())
         return ans
 
-    def visitDeclaration(self, ctx):
-        if ctx.initDeclaratorList():
-            return self.visit(ctx.declarationSpecifiers()) + ' ' + self.visit(ctx.initDeclaratorList()) + ';'
-        return self.visit(ctx.declarationSpecifiers()) + ';'
-
-    def visitDeclarationSpecifiers(self, ctx):
-        datas = ctx.declarationSpecifier()
-        ans = ''
-        started = False
-        for data in datas:
-            if started:
-                ans += ' ' + self.visit(data)
-            else:
-                started = True
-                ans += self.visit(data)
-        return ans
-
-    def visitDeclarationSpecifiers2(self, ctx):
-        datas = ctx.declarationSpecifier()
-        ans = ''
-        started = False
-        for data in datas:
-            if started:
-                ans += ' ' + self.visit(data)
-            else:
-                started = True
-                ans += self.visit(data)
-        return ans
-
-    def visitDeclarationSpecifier(self, ctx):
-        if ctx.typeSpecifier():
-            return self.visit(ctx.typeSpecifier())
-        return self.visit(ctx.typeQualifier())
-
     def visitTypeSpecifier(self, ctx):
-        # if ctx.typedefName():
-        #     return self.visit(ctx.typedefName())
-        # if ctx.typeSpecifier():
-        #     return self.visit(ctx.typeSpecifier()) + ' ' + self.visit(ctx.pointer())
-        # return ctx.getText()
+        if ctx.CONST():
+            return 'const'
         return 'let'
 
-    def visitTypeQualifier(self, ctx):
-        return ctx.getText()
-
-    def visitTypedefName(self, ctx):
-        return ctx.Identifier().getText()
-    
-    def visitPointer(self, ctx):
-        ans = '*'
-        if ctx.typeQualifierList():
-            ans += ' ' + self.visit(ctx.typeQualifierList())
-        if ctx.pointer():
-            ans += ' ' + self.visit(ctx.pointer())
-        return ans
-
-    def visitTypeQualifierList(self, ctx):
-        return "visitTypeQualifierList"
-
     def visitDeclarator(self, ctx):
-        return "visitdeclarator"
-    
-    def visitDeclarationList(self, ctx):
-        if ctx.declarationList():
-            return self.visit(ctx.declarationList()) + ' ' + self.visit(ctx.declaration())
-        return self.visit(ctx.declaration())
+        return ctx.Identifier().getText()
+
+    def visitDeclaration(self, ctx):
+        return self.visit(ctx.typeSpecifier()) + ' ' + self.visit(ctx.initDeclaratorList()) + ';'
 
     def visitCompoundStatement(self, ctx):
-        return "visitCompoundStatement"
+        return '{\n' + addIndex('\n'.join([self.visit(i) for i in ctx.children[1:-1]])) + '\n}'
+
+    def visitCompoundStatementInMain(self, ctx):
+        return '\n'.join([self.visit(i) for i in ctx.children[1:-1]])
+
+    def visitBlockItem(self, ctx):
+        if ctx.statement():
+            return self.visit(ctx.statement())
+        return self.visit(ctx.declaration())
 
     def visitInitDeclaratorList(self, ctx):
-        return "VISITinitDeclaratorList"
+        return ', '.join([self.visit(i) for i in ctx.initDeclarator()])
+
+    def visitInitDeclarator(self, ctx):
+        if ctx.initializer():
+            return self.visit(ctx.declarator()) + ' = ' + self.visit(ctx.initializer())
+        return self.visit(ctx.declarator())
+
+    def visitStatement(self, ctx):
+        return "visitStatement"
+
+    def visitInitializer(self, ctx):
+        return "visitInitializer"
 
 
 def main(argv):
